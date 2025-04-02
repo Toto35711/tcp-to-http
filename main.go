35dc55b7a5dced1db86474d15a61dd25054e79e2
Toto35711 <toto2441999@gmail.com>
2025-04-02 19:01:09 +0700

    feat: add tcp listener and very basic connection handler

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello")
+	listener, err := net.Listen("tcp",":8080")
+
+	if err != nil {
+		log.Fatal("can not establish a listener: ", err)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error in accepting connection: ", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("can not read network connection", err)
+	}
+	strBuf := string(buf)
+	fmt.Println(strBuf)
+	defer conn.Close()
 }
