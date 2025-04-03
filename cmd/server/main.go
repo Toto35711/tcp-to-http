ad64a50854bacdd335004f699edaf3b39bfe7f1e
Toto35711 <toto2441999@gmail.com>
2025-04-03 10:03:45 +0700

    feat: add sample http response

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -26,12 +26,14 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	buf := make([]byte, 1024)
+	buf := make([]byte, 4096)
 	_, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("can not read network connection", err)
 	}
 	strBuf := string(buf)
 	fmt.Println(strBuf)
+	response := "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nNice!"
+	conn.Write([]byte(response))
 	defer conn.Close()
 }
